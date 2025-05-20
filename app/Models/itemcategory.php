55f6d811dd799d69931c97552a20714c540bb955
jalalon11 <vincentjhanrey.jalalon@gmail.com>
2025-05-20 15:14:14 +0800

    Add explicit table names to Supplier_Items and ItemCategory models

--- app/Models/itemcategory.php
+++ app/Models/itemcategory.php
@@ -5,9 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class itemcategory extends Model
+class ItemCategory extends Model
 {
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'itemcategories';
 
     protected $fillable = ['category', 'id'];
 }
--- app/Models/supplier_items.php
+++ app/Models/supplier_items.php
@@ -5,9 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class supplier_items extends Model
+class Supplier_Items extends Model
 {
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'supplier_items';
 
     protected $fillable = ['id', 'supplier_id', 'item_id', 'stock', 'serialnumber', 'modelnumber', 'no_ofYears', 'category_id', 'quantity',
     'cost', 'totalCost','status','type','remarks', 'date',];
